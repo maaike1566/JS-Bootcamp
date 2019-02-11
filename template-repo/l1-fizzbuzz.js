8cbd2569d5212debf291c8bd8e4128c0728ae9fb
maaike1566 <45418184+maaike1566@users.noreply.github.com>
2019-02-11 16:42:47 +0100

    Update l1-fizzbuzz.js

--- template-repo/l1-fizzbuzz.js
+++ template-repo/l1-fizzbuzz.js
@@ -1,1 +1,17 @@
-r
+for (var number = 1; number <= 100; number++){
+var leeg = " "
+    if (number % 3 == 0 && number % 5 == 0){
+        leeg += "Fizzbuzz";
+    }
+    else if (number % 3 == 0){
+        leeg += "Fizz";
+    }
+    else if( number % 5 == 0){
+        leeg += "Buzz";
+    }
+    else{
+        leeg += number;
+    }
+
+    console.log(leeg);
+}
