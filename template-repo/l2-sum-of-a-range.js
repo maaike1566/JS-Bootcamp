e534c3e47d171772e1d2bd43d1d64aa1445f21e0
maaike1566 <45418184+maaike1566@users.noreply.github.com>
2019-02-15 22:40:21 +0100

    Update l2-sum-of-a-range.js

--- template-repo/l2-sum-of-a-range.js
+++ template-repo/l2-sum-of-a-range.js
@@ -1,1 +1,23 @@
- 
+ function range(start, end, step = start < end ? 1 : -1) {
+  var array = [];
+
+if (step > 0) {
+    for (var i = start; i <= end; i += step) array.push(i);
+}
+else {
+    for (var i = start; i >= end; i += step) array.push(i);
+}
+    return array;
+}
+
+function sum(array) {
+    var total = 0;
+    for (let value of array) {
+        total += value;
+    }
+    return total;
+}
+
+console.log(range(1, 10))
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
