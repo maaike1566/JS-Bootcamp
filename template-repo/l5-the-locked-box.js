8b315beda745e943c432439922c3de215b2b55ff
maaike1566 <45418184+maaike1566@users.noreply.github.com>
2019-02-15 23:31:28 +0100

    Update l5-the-locked-box.js

--- template-repo/l5-the-locked-box.js
+++ template-repo/l5-the-locked-box.js
@@ -1,1 +1,38 @@
- 
+ var box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  content2: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this.content2;
+  }
+};
+
+function withBoxUnlocked(body) {
+  var locked = box.locked;
+  if (!locked) {
+    return body();
+  }
+
+  box.unlock();
+  try {
+    return body();
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+
+console.log(box.locked);
