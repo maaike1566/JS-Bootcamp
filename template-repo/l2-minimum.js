da351c9a4a1f1bd5996e378e0bb792e381d20196
maaike1566 <45418184+maaike1566@users.noreply.github.com>
2019-02-15 22:29:55 +0100

    Update l2-minimum.js

--- template-repo/l2-minimum.js
+++ template-repo/l2-minimum.js
@@ -9,14 +9,3 @@ function min(a, b) {
 
 console.log(min(120, 5));
 
-//recursion 
-function isEven(x) {
-  if (x == 0) return true;
-  else if (x == 1) return false;
-  else if (x < 0) return isEven(-x);
-  else return isEven(x - 2);
-}
-
-console.log(isEven(50));
-console.log(isEven(75));
-console.log(isEven(-1));
